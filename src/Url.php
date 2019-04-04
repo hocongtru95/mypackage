838fb84e9047cdc82af1c9d9d87b0e118ad93067
Cong Tru <hocongtru95@gmail.com>
2019-04-04 13:24:13 +0700

    commit 2

--- src/Url.php
+++ src/Url.php
@@ -5,6 +5,7 @@ class Url
 {
     public function sluggify($string, $separator = '-', $maxLength = 96)
     {
+        //this my change
         $title = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $string);
         $title = preg_replace('%[^-/+|\w ]%', '', $title);
         $title = strtolower(trim(substr($title, 0, $maxLength), '-'));
